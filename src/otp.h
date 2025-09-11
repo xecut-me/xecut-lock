08c6a82b183796fe313038cab5f375024159e2c8
kirill9617 <kirill9617@yandex.ru>
2025-09-11 21:57:29 +0200

    refactor otp and kdf

--- src/otp.h
+++ src/otp.h
@@ -1,13 +1,9 @@
 #pragma once
 
 #include <stdbool.h>
+#include <stdint.h>
 
-struct otp_key_info {
-    char name[16];
-    uint8_t key[128];
-    uint8_t key_len;
-    uint8_t digits;
-    bool valid;
-    uint32_t code;
-    uint64_t step;
-};
+
+bool otp_verify_kdf(char *uid, size_t uid_len, char *code, size_t code_len);
+
+int otp_init();
