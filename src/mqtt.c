62fa78b4189dfc89f0bdb1bc420d52aab07e7a6f
Valerii Klachkov <vklachkov@pm.me>
2025-09-19 12:49:12 +0200

    Group mqtt state to one struct

--- src/mqtt.c
+++ src/mqtt.c
@@ -13,17 +13,82 @@ LOG_MODULE_REGISTER(MQTT);
 
 #define CLIENT_ID "xecut_lock"
 
-static struct sockaddr_storage broker;
+static void client_setup(const char *user_name, const char *password);
+static void client_broker_setup(const char *ip_addr, uint16_t port);
+static void client_connected(void);
+static void client_disconnected(void);
 
-static uint8_t rx_buffer[256];
-static uint8_t tx_buffer[256];
+static void mqtt_evt_handler(
+    struct mqtt_client *client,
+    const struct mqtt_evt *evt
+);
 
-static struct mqtt_client client;
-static struct pollfd fd;
-static bool connected;
+struct mqtt_manager {
+    struct sockaddr_storage broker;
 
-static void clear_fds(void) {
-    fd.fd = -1;
+    uint8_t rx_buffer[256];
+    uint8_t tx_buffer[256];
+    struct pollfd fd;
+
+    struct mqtt_client client;
+    bool connected;
+};
+
+static struct mqtt_manager client = {0};
+
+static void client_setup(const char *user_name, const char *password) {
+#define C client.client
+
+    mqtt_client_init(&C);
+
+    C.broker = &client.broker;
+
+    C.protocol_version = MQTT_VERSION_3_1_1;
+    C.transport.type = MQTT_TRANSPORT_NON_SECURE;
+
+    C.evt_cb = mqtt_evt_handler;
+
+    C.client_id.utf8 = (uint8_t *)CLIENT_ID;
+    C.client_id.size = sizeof(CLIENT_ID) - 1;
+
+    C.password = NULL;
+    if (password != NULL) {
+        C.password = k_malloc(sizeof(struct mqtt_utf8));
+        C.password->utf8 = (uint8_t *)password;
+        C.password->size = strlen(password);
+    }
+
+    C.user_name = NULL;
+    if (user_name != NULL) {
+        C.user_name = k_malloc(sizeof(struct mqtt_utf8));
+        C.user_name->utf8 = (uint8_t *)user_name;
+        C.user_name->size = strlen(user_name);
+    }
+
+    C.rx_buf = (uint8_t *)&client.rx_buffer;
+    C.rx_buf_size = sizeof(client.rx_buffer);
+
+    C.tx_buf = (uint8_t *)&client.tx_buffer;
+    C.tx_buf_size = sizeof(client.tx_buffer);
+
+#undef C
+}
+
+static void client_broker_setup(const char *ip_addr, uint16_t port) {
+    struct sockaddr_in *addr = (struct sockaddr_in *)&client.broker;
+	addr->sin_family = AF_INET;
+	addr->sin_port = htons(port);
+	inet_pton(AF_INET, ip_addr, &addr->sin_addr);
+}
+
+static void client_connected(void) {
+    client.connected = true;
+}
+
+static void client_disconnected(void) {
+    client.connected = false;
+    client.fd.fd = -1;
+    client.fd.events = 0;
 }
 
 static void mqtt_evt_handler(
@@ -33,7 +98,7 @@ static void mqtt_evt_handler(
     switch (evt->type) {
         case MQTT_EVT_CONNACK:
             if (evt->result == 0) {
-                connected = true;
+                client_connected();
             } else {
                 LOG_WRN("MQTT connection failed with status code %d", evt->result);
             }
@@ -41,8 +106,7 @@ static void mqtt_evt_handler(
     
         case MQTT_EVT_DISCONNECT:
             LOG_WRN("MQTT client disconnected with status code %d", evt->result);
-            connected = false;
-            clear_fds();
+            client_disconnected();
             break;
 
         case MQTT_EVT_PUBREC:
@@ -67,60 +131,19 @@ static void mqtt_evt_handler(
     }
 }
 
-static void broker_init(
-    const char *ip_addr,
-    uint16_t port
-) {
-    struct sockaddr_in *addr = (struct sockaddr_in *)&broker;
-	addr->sin_family = AF_INET;
-	addr->sin_port = htons(port);
-	inet_pton(AF_INET, ip_addr, &addr->sin_addr);
-}
-
 void mqtt_init(
     const char *ip_addr,
     uint16_t port,
     const char *user_name,
     const char *password
 ) {
-    mqtt_client_init(&client);
-
-    broker_init(ip_addr, port);
-
-    client.broker = &broker;
-
-    client.protocol_version = MQTT_VERSION_3_1_1;
-    client.transport.type = MQTT_TRANSPORT_NON_SECURE;
-
-    client.evt_cb = mqtt_evt_handler;
-
-    client.client_id.utf8 = (uint8_t *)CLIENT_ID;
-    client.client_id.size = sizeof(CLIENT_ID) - 1;
-
-    client.password = NULL;
-    if (password != NULL) {
-        client.password = k_malloc(sizeof(struct mqtt_utf8));
-        client.password->utf8 = (uint8_t *)password;
-        client.password->size = strlen(password);
-    }
-
-    client.user_name = NULL;
-    if (user_name != NULL) {
-        client.user_name = k_malloc(sizeof(struct mqtt_utf8));
-        client.user_name->utf8 = (uint8_t *)user_name;
-        client.user_name->size = strlen(user_name);
-    }
-
-    client.rx_buf = rx_buffer;
-    client.rx_buf_size = sizeof(rx_buffer);
-
-    client.tx_buf = tx_buffer;
-    client.tx_buf_size = sizeof(tx_buffer);
+    client_broker_setup(ip_addr, port);
+    client_setup(user_name, password);
 }
 
 void prepare_fd(void) {
-    fd.fd = client.transport.tcp.sock;
-    fd.events = POLLIN;
+    client.fd.fd = client.client.transport.tcp.sock;
+    client.fd.events = POLLIN;
 }
 
 void mqtt_run(void) {
@@ -129,7 +152,7 @@ void mqtt_run(void) {
     int max_try = 3;
 
     while (try++ < max_try) {
-        ret = mqtt_connect(&client);
+        ret = mqtt_connect(&client.client);
 		if (ret != 0) {
 			LOG_WRN("Failed to connect to MQTT broker: %d", ret);
 			k_sleep(K_MSEC(1000));
@@ -138,23 +161,23 @@ void mqtt_run(void) {
 
         prepare_fd();
 
-        ret = poll(&fd, 1, 2000);
+        ret = poll(&client.fd, 1, 2000);
         if (ret < 0) {
 			LOG_WRN("Failed to poll MQTT broker: %d", ret);
 			k_sleep(K_MSEC(1000));
 			continue;
         }
 
-        mqtt_input(&client);
+        mqtt_input(&client.client);
 
-        if (connected) {
+        if (client.connected) {
             break;
         } else {
-			mqtt_abort(&client);
+			mqtt_abort(&client.client);
 		}
     }
 
-    if (!connected) {
+    if (!client.connected) {
         LOG_ERR("Meh(((");
     }
 }
@@ -169,16 +192,16 @@ static int publish(const char *topic, const char *payload, enum mqtt_qos qos) {
 	param.message.payload.data = (uint8_t *)payload;
 	param.message.payload.len = strlen(payload);
 	param.message_id = sys_rand16_get();
-	param.dup_flag = 0U;
-	param.retain_flag = 0U;
+	param.dup_flag = 0;
+	param.retain_flag = 0;
 
-	return mqtt_publish(&client, &param);
+	return mqtt_publish(&client.client, &param);
 }
 
 void mqtt_test(void) {
     int ret;
 
-    ret = mqtt_ping(&client);
+    ret = mqtt_ping(&client.client);
     LOG_INF("Ping result: %d", ret);
 
     char buffer[256] = {0};
